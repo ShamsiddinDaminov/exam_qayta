ec2b69bc7b6988a5e502c2bb06d8d1c80092caec
ShamsiddiDaminov <shamsiddinpython@gmail.com>
2024-01-13 10:43:03 +0500

    add javob 4

--- javob_4/main.py
+++ javob_4/main.py
@@ -7,7 +7,7 @@ from aiogram.filters import CommandStart, Command
 from aiogram.fsm.storage.memory import MemoryStorage
 from bs4 import BeautifulSoup
 
-BOT_TOKEN = "6926663249:AAFSfW2XTFUMmXtbZO0YjYYRtAae0_hrAIQ"
+BOT_TOKEN = "6949726231:AAGJ69DbWuw1GNBavd7tOiKxUXy4Opqk_1s"
 dp = Dispatcher(storage=MemoryStorage())
 
 
